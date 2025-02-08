7e321d2551e927256c3c1f988699a345b3e70f15
xbx18502 <xbx18502@gmail.com>
2025-02-08 18:04:30 +0900

    edit ClientMain

---
+++ raft-java-example/src/main/java/com/github/raftimpl/raft/example/client/ClientMain_Test.java
@@ -0,0 +1,41 @@
+package com.github.raftimpl.raft.example.client;
+
+import com.baidu.brpc.client.BrpcProxy;
+import com.baidu.brpc.client.RpcClient;
+import com.github.raftimpl.raft.example.server.service.ExampleProto;
+import com.github.raftimpl.raft.example.server.service.ExampleService;
+import com.googlecode.protobuf.format.JsonFormat;
+
+public class ClientMain_Test {
+    public static void main(String[] args) {
+
+//        String ipPorts = "list://192.168.91.134:8051,192.168.91.134:8052,192.168.91.134:8053";
+        String ipport = "list://192.168.91.134:8053";
+        String key = "username123";
+        String value =null;
+
+        // init rpc client
+        RpcClient rpcClient = new RpcClient(ipport);
+
+        ExampleService exampleService = BrpcProxy.getProxy(rpcClient, ExampleService.class);
+        final JsonFormat jsonFormat = new JsonFormat();
+
+        // set
+        if (value != null) {
+            ExampleProto.SetRequest setRequest = ExampleProto.SetRequest.newBuilder()
+                    .setKey(key).setValue(value).build();
+            ExampleProto.SetResponse setResponse = exampleService.set(setRequest);
+            System.out.printf("set request, key=%s value=%s response=%s\n",
+                    key, value, jsonFormat.printToString(setResponse));
+        } else {
+            // get
+            ExampleProto.GetRequest getRequest = ExampleProto.GetRequest.newBuilder()
+                    .setKey(key).build();
+            ExampleProto.GetResponse getResponse = exampleService.get(getRequest);
+            System.out.printf("get request, key=%s, response=%s\n",
+                    key, jsonFormat.printToString(getResponse));
+        }
+
+        rpcClient.stop();
+    }
+}
